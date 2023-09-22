fe348c76abda1bc07053476df659dadeee870af2
pavelxhukin <pavel_xhukin@mail.ru>
2023-09-22 11:52:08 +0300

    add Hand class

---
+++ Hand.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace Poker
+{
+    class Hand
+    {
+        List<Card> cardHand = new List<Card>();
+
+        private void FillHand(CardDeck deck, int number)
+        {
+            for(int i = 0; i< number; i++){
+               cardHand.Add(deck.GetCard()); 
+            }
+        }
+
+        public Hand(CardDeck deck, int number)
+        {
+            FillHand(deck);			
+        }
+
+        public void AddCardToHand(Card card){
+            cardHand.Add(card);
+        }
+
+        public void AddHandToHand(List<Card> cardList){
+            cardHand // cardHand = cardHand + cardList
+        }
+       
+        // тут по идее метод для оценки стоимости (использовать когда рука = 7)
+    }
+}
