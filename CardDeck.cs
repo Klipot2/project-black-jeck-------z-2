f867132ac2449f1b9407627180952816f75139be
Klipot <nikita.murashov2004@gmail.com>
2023-09-22 11:08:14 +0300

    some text

--- CardDeck.cs
+++ CardDeck.cs
@@ -1,56 +1,82 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace Poker
 {
-	class CardDeck
-	{	
-		public enum allSuits
-		{
-			S,	// Spades
-			H,	// Hearts	
-			C,	// Clubs
-			D	// Diamonds
-		}
+    class CardDeck
+    {
+        public enum AllSuits
+        {
+            S,  // Spades
+            H,  // Hearts  
+            C,  // Clubs
+            D   // Diamonds
+        }
 
-		public enum allRanks
-		{
-			two = 2,
-			three,
-			four,
-			five,
-			six,
-			seven,
-			eight,
-			nine,
-			ten,
-			J, //Jack
-			Q,	//Queen
-			K,	//King
-			A	// Ace
-		}
+        public enum AllRanks
+        {
+            Two = '2',
+            Three = '3',
+            Four = '4',
+            Five = '5',
+            Six = '6',
+            Seven = '7',
+            Eight = '8',
+            Nine = '9',
+            Ten = 'T',
+            J = 'J', // Jack
+            Q = 'Q', // Queen
+            K = 'K', // King
+            A = 'A'  // Ace
+        }
 
-		List<Card> cardDeck = new List<Card>();
+        List<Card> cardDeck = new List<Card>();
 
-		
-		private void fillDeck(){
-			foreach(var suit in allSuits)
-			{
-				foreach(var rank in allRanks)
-				{
-					Card temporaryCard = new Card(rank,suit);
-					cardDeck.Add(temporaryCard);
-				}
-			}
-		}
+        private void FillDeck()
+        {
+            foreach (var suit in Enum.GetValues(typeof(AllSuits)))
+            {
+                foreach (var rank in Enum.GetValues(typeof(AllRanks)))
+                {
+                    Card temporaryCard = new Card((char)rank, (char)suit);
+                    cardDeck.Add(temporaryCard);
+                }
+            }
+        }
 
-		public CardDeck(){
-			fillDeck()
-		}	
-		public void writeDeck(){
-			Console.WriteLine(cardDeck);
-		}
-	}
+        public CardDeck()
+        {
+            FillDeck();
+        }
+
+        public void WriteDeck()
+        {
+            foreach (var card in cardDeck)
+            {
+                Console.WriteLine($"{card.GetRank()} of {card.GetSuit()}");
+            }
+        }
+
+        public List<Card> GetShuffledDeck()
+        {
+            List<Card> shuffledDeck = new List<Card>(cardDeck);
+            // реализовать логику тасования колоды.
+            ShuffleDeck(shuffledDeck);
+            return shuffledDeck;
+        }
+
+        private void ShuffleDeck(List<Card> deck)
+        {
+            Random rng = new Random();
+            int n = deck.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                Card value = deck[k];
+                deck[k] = deck[n];
+                deck[n] = value;
+            }
+        }
+    }
 }
