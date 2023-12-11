310c933c812a3d11777860ec285213efce9f4b08
Klipot2 <144228940+Klipot2@users.noreply.github.com>
2023-12-11 12:15:59 +0100

    Update DrawCards.cs

--- DrawCards.cs
+++ DrawCards.cs
@@ -1,92 +1,98 @@
-public static void DrawCards(List<Card> cards, int cardsPerLine)
+namespace Casino.CardGames.Poker
 {
-    int totalCards = cards.Count;
-    int totalLines = (int)Math.Ceiling((double)totalCards / cardsPerLine);
-
-    for (int line = 0; line < totalLines; line++)
+    public static class DrawCards
     {
-        DrawTopLine(cards, line, cardsPerLine);
-        DrawCardContent(cards, line, cardsPerLine);
-        DrawBottomLine(cards, line, cardsPerLine);
-    }
-}
-
-private static void DrawTopLine(List<Card> cards, int line, int cardsPerLine)
-{
-    for (int i = 0; i < cardsPerLine; i++)
-    {
-        int cardIndex = line * cardsPerLine + i;
-        if (cardIndex < cards.Count)
+        public static void DrawCards(List<Card> cards, int cardsPerLine)
         {
-            Console.Write(" ____  ");
+            int totalCards = cards.Count;
+            int totalLines = (int)Math.Ceiling((double)totalCards / cardsPerLine);
+
+            for (int line = 0; line < totalLines; line++)
+            {
+                DrawTopLine(cards, line, cardsPerLine);
+                DrawCardContent(cards, line, cardsPerLine);
+                DrawBottomLine(cards, line, cardsPerLine);
+            }
+        }
+
+        private static void DrawTopLine(List<Card> cards, int line, int cardsPerLine)
+        {
+            for (int i = 0; i < cardsPerLine; i++)
+            {
+                int cardIndex = line * cardsPerLine + i;
+                if (cardIndex < cards.Count)
+                {
+                    Console.Write(" ____  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void DrawCardContent(List<Card> cards, int line, int cardsPerLine)
+        {
+            for (int i = 0; i < cardsPerLine; i++)
+            {
+                int cardIndex = line * cardsPerLine + i;
+                if (cardIndex < cards.Count)
+                {
+                    DrawCard(cards[cardIndex]);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void DrawBottomLine(List<Card> cards, int line, int cardsPerLine)
+        {
+            for (int i = 0; i < cardsPerLine; i++)
+            {
+                int cardIndex = line * cardsPerLine + i;
+                if (cardIndex < cards.Count)
+                {
+                    Console.Write("|    | ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static void DrawCard(Card card)
+        {
+            Console.Write("|");
+
+            string valueString = card.CardValue switch
+            {
+                Card.Value.Ten => "10",
+                _ => card.CardValue.ToString().Substring(0, 1)
+            };
+
+            Console.Write($"{valueString.PadRight(4)}");
+
+            // Draw the suit
+            DrawCardSuitValue(card);
+
+            Console.Write("| ");
+        }
+
+        private static void DrawCardSuitValue(Card card)
+        {
+            char cardSuit = ' ';
+
+            switch (card.CardSuit)
+            {
+                case Card.Suit.H:
+                    cardSuit = '\u2665';
+                    break;
+                case Card.Suit.D:
+                    cardSuit = '\u2666';
+                    break;
+                case Card.Suit.C:
+                    cardSuit = '\u2663';
+                    break;
+                case Card.Suit.S:
+                    cardSuit = '\u2660';
+                    break;
+            }
+
+            Console.Write($"{cardSuit}  ");
         }
     }
-    Console.WriteLine();
-}
-
-private static void DrawCardContent(List<Card> cards, int line, int cardsPerLine)
-{
-    for (int i = 0; i < cardsPerLine; i++)
-    {
-        int cardIndex = line * cardsPerLine + i;
-        if (cardIndex < cards.Count)
-        {
-            DrawCard(cards[cardIndex]);
-        }
-    }
-    Console.WriteLine();
-}
-
-private static void DrawBottomLine(List<Card> cards, int line, int cardsPerLine)
-{
-    for (int i = 0; i < cardsPerLine; i++)
-    {
-        int cardIndex = line * cardsPerLine + i;
-        if (cardIndex < cards.Count)
-        {
-            Console.Write("|    | ");
-        }
-    }
-    Console.WriteLine();
-}
-
-public static void DrawCard(Card card)
-{
-    Console.Write("|");
-
-    string valueString = card.CardValue switch
-    {
-        Card.Value.Ten => "10",
-        _ => card.CardValue.ToString().Substring(0, 1)
-    };
-
-    Console.Write($"{valueString.PadRight(4)}");
-
-    // Draw the suit
-    DrawCardSuitValue(card);
-
-    Console.Write("| ");
-}
-
-private static void DrawCardSuitValue(Card card)
-{
-    char cardSuit = ' ';
-
-    switch (card.CardSuit)
-    {
-        case Card.Suit.H:
-            cardSuit = '\u2665';
-            break;
-        case Card.Suit.D:
-            cardSuit = '\u2666';
-            break;
-        case Card.Suit.C:
-            cardSuit = '\u2663';
-            break;
-        case Card.Suit.S:
-            cardSuit = '\u2660';
-            break;
-    }
-
-    Console.Write($"{cardSuit}  ");
 }
