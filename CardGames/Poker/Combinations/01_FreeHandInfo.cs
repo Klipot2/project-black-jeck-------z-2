82fb022ac4a8a0f6777705fdcb5db25b2fd8ea5f
Klipot2 <144228940+Klipot2@users.noreply.github.com>
2024-02-19 11:52:02 +0100

    Update 01_FreeHandInfo.cs
    add summary

--- CardGames/Poker/Combinations/01_FreeHandInfo.cs
+++ CardGames/Poker/Combinations/01_FreeHandInfo.cs
@@ -1,9 +1,20 @@
 namespace Casino.CardGames.Poker.Combinations
 {
+    /// <summary>
+    /// Represents a free hand, which always indicates the presence of a hand.
+    /// </summary>
     public class FreeHandInfo : CombinationInfo
     {
-        public FreeHandInfo(List<Card> cards) : base(cards){}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FreeHandInfo"/> class.
+        /// </summary>
+        /// <param name="cards">List of cards in the hand.</param>
+        public FreeHandInfo(List<Card> cards) : base(cards) { }
 
+        /// <summary>
+        /// Checks if the free hand combination is always present.
+        /// </summary>
+        /// <returns>Always returns true.</returns>
         protected override bool IsCombinationPresent() => true;
     }
 }
