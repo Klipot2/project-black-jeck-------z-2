8776dc438518daf986dda3e4237f0804eb3241c6
Klipot2 <144228940+Klipot2@users.noreply.github.com>
2024-02-19 11:24:27 +0100

    Update Card.cs
    edit summery

--- CardGames/Card.cs
+++ CardGames/Card.cs
@@ -1,28 +1,47 @@
 namespace Casino.CardGames
 {
+    /// <summary>
+    /// Represents a standard playing card.
+    /// </summary>
     public class Card
     {
-        /// <summary> Suit of a card.</summary>
+        /// <summary> 
+        /// Suit of a card.
+        /// </summary>
         public enum Suit
         {
-            S,
-            H,
-            C,
-            D
+            S,  // Spades
+            H,  // Hearts
+            C,  // Clubs
+            D   // Diamonds
         }
 
-        /// <summary> Value of a card.</summary>
+        /// <summary> 
+        /// Value of a card.
+        /// </summary>
         public enum Value
         {
             Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
         }
 
+        /// <summary> 
+        /// Gets the suit of the card.
+        /// </summary>
         public Suit CardSuit { get { return _suit; } }
+
+        /// <summary> 
+        /// Gets the value of the card.
+        /// </summary>
         public Value CardValue { get { return _value; } }
 
         private readonly Suit _suit;
         private readonly Value _value;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Card"/> class.
+        /// </summary>
+        /// <param name="suit">The suit of the card.</param>
+        /// <param name="value">The value of the card.</param>
         public Card(Suit suit, Value value)
         {
             _suit = suit;
