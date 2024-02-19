880c58987c1bddc750e260a38db4e8ca194f9a48
Klipot2 <144228940+Klipot2@users.noreply.github.com>
2024-02-19 11:42:21 +0100

    Update FiveCardPoker.cs
    add summary

--- CardGames/Poker/FiveCardPoker.cs
+++ CardGames/Poker/FiveCardPoker.cs
@@ -3,6 +3,9 @@ using Casino.CardGames.Poker.Combinations;
 
 namespace Casino.CardGames.Poker
 {
+    /// <summary>
+    /// Represents a five-card poker game.
+    /// </summary>
     public class FiveCardPoker : IPlayable
     {
         private const int PLAYER_HAND_SIZE = 5;
@@ -10,7 +13,6 @@ namespace Casino.CardGames.Poker
         private const int DEALER_LEEWAY = 2;
         private const int MIN_BET = 10;
         private const int INITIAL_PLAYER_BANK = 1000;
-
 
         private readonly PokerHand _playerHand;
         private readonly PokerHand _dealerHand;
@@ -21,18 +23,23 @@ namespace Casino.CardGames.Poker
         private readonly List<int> _swapArray;
         private List<int> _possibleSwapInputs;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FiveCardPoker"/> class.
+        /// </summary>
         public FiveCardPoker()
         {
             _playerHand = new PokerHand("Player", PLAYER_HAND_SIZE);
             _dealerHand = new PokerHand("Dealer", PLAYER_HAND_SIZE);
             _deck = new DeckCards();
-            _playerBank = INITIAL_PLAYER_BANK; 
+            _playerBank = INITIAL_PLAYER_BANK;
 
-
-            _swapArray = [];
-            _possibleSwapInputs = [];
+            _swapArray = new List<int>();
+            _possibleSwapInputs = new List<int>();
         }
 
+        /// <summary>
+        /// Plays a round of the five-card poker game.
+        /// </summary>
         public void Play()
         {
             _deck.SetUpDeck();
@@ -42,29 +49,30 @@ namespace Casino.CardGames.Poker
             int currentBet = MIN_BET;
             _playerBank -= currentBet;
             _tableBank = currentBet * 2;
-            
 
             PokerUIHandler.DisplayPlayerStatus(_playerHand, _playerBank, currentBet);
-            // Dealer decides if they want to raise
-            // Player response
-            // Dealer response (if needed)
+
+            Input[] possibleInputs = { Input.Yes, Input.No };
             
-            Input[] possibleInputs = [Input.Yes, Input.No];
+            // Player decides whether to swap cards
             PokerUIHandler.MessageWithInputResponse("Do you want to swap any cards? Press (Y)es or (N)o.",
                 possibleInputs, "Press 'Y' to swap cards, or 'N' to skip.", LaunchCardSwap);
+
+            // Swap cards based on player's choice
             foreach (var cardPosition in _swapArray)
             {
                 Card newCard = _deck.DrawCard();
-                // CardRenderer.PrintCards([_playerHand.GetCards()[cardPosition - 1], newCard]);
                 _playerHand.PlainSwapCard(cardPosition - 1, newCard);
             }
+
             _playerHand.ForceUpdateHandValue();
             PokerUIHandler.DisplayPlayerStatus(_playerHand, _playerBank, currentBet);
 
             // Player raises bet or not
             // Dealer response (if needed)
-
             bool playerHasBetterHand = _playerHand.Value > _dealerHand.Value;
+            
+            // Display game result
             if (playerHasBetterHand)
             {
                 Console.WriteLine("You won!");
@@ -74,56 +82,46 @@ namespace Casino.CardGames.Poker
                 Console.WriteLine("Dealer won!");
             }
 
+            // Ask if the player wants to play another game
             PokerUIHandler.MessageWithInputResponse("Do you want to play another game? Press (Y)es or (N)o.",
                 possibleInputs, "Press 'Y' to start another game, or 'N' to close the program.", RestartSequence);
         }
 
+        /// <summary>
+        /// Deals cards to the dealer, evaluates the dealer's hand, and displays it.
+        /// </summary>
         private void DealToDealer()
         {
             _dealerHand.ResetHand();
             List<Card> dealtCards = _deck.DrawCards(PLAYER_HAND_SIZE + DEALER_LEEWAY);
-            // List<Card> dealtCards =
-            // [
-            //     new Card(Card.Suit.D, Card.Value.Queen),
-            //     new Card(Card.Suit.D, Card.Value.Jack),
-            //     new Card(Card.Suit.D, Card.Value.King),
-            //     new Card(Card.Suit.C, Card.Value.Nine),
-            //     new Card(Card.Suit.C, Card.Value.Ace),
-            //     new Card(Card.Suit.D, Card.Value.Ace),
-            //     new Card(Card.Suit.C, Card.Value.Six),
-            //     new Card(Card.Suit.D, Card.Value.Ten),
-            //     new Card(Card.Suit.C, Card.Value.Four),
-            //     new Card(Card.Suit.C, Card.Value.Jack),
-            //     new Card(Card.Suit.C, Card.Value.Two),
-            //     new Card(Card.Suit.C, Card.Value.Three),
-            // ];
+
             ValueData expandedHandValue = new(dealtCards.Count, true);
             HandEvaluator.CalculateHandValueData(dealtCards, expandedHandValue);
             _dealerHand.AddCards(dealtCards.GetRange(0, PLAYER_HAND_SIZE));
+
             for (int i = PLAYER_HAND_SIZE; i < dealtCards.Count; i++)
             {
                 _deck.AddCard(dealtCards[i]);
             }
-            _deck.ShuffleCards();
 
+            _deck.ShuffleCards();
             PokerUIHandler.DisplayHand(_dealerHand);
         }
 
+        /// <summary>
+        /// Deals cards to the player.
+        /// </summary>
         private void DealToPlayer()
         {
             _playerHand.ResetHand();
             List<Card> dealtCards = _deck.DrawCards(PLAYER_HAND_SIZE);
-            // List<Card> dealtCards =
-            // [
-            //     new Card(Card.Suit.C, Card.Value.Jack),
-            //     new Card(Card.Suit.C, Card.Value.Five),
-            //     new Card(Card.Suit.H, Card.Value.Four),
-            //     new Card(Card.Suit.D, Card.Value.Eight),
-            //     new Card(Card.Suit.D, Card.Value.Ace)
-            // ];
             _playerHand.AddCards(dealtCards);
         }
 
+        /// <summary>
+        /// Initiates the card swapping process based on the player's choice.
+        /// </summary>
+        /// <param name="input">The player's input.</param>
         private void LaunchCardSwap(Input input)
         {
             switch (input)
@@ -145,19 +143,23 @@ namespace Casino.CardGames.Poker
                     break;
                 default:
                     throw new ArgumentException(
-                    string.Format("Cannot recognize {0} input for LaunchCardSwap.",
-                    input));
+                        string.Format("Cannot recognize {0} input for LaunchCardSwap.",
+                        input));
             }
         }
 
+        /// <summary>
+        /// Processes each step of the card swapping based on player's input.
+        /// </summary>
+        /// <param name="inputInt">The numeric input from the player.</param>
         private void ProcessCardSwapStep(int inputInt)
         {
             if (inputInt == CARD_SWAP_TERMINATOR) return;
 
             if (inputInt < 1 || inputInt > PLAYER_HAND_SIZE)
                 throw new ArgumentOutOfRangeException(
-                string.Format("Input for CardSwap is not {0} and not between 1 and {1}.",
-                CARD_SWAP_TERMINATOR, PLAYER_HAND_SIZE));
+                    string.Format("Input for CardSwap is not {0} and not between 1 and {1}.",
+                    CARD_SWAP_TERMINATOR, PLAYER_HAND_SIZE));
 
             if (!_swapArray.Remove(inputInt))
             {
@@ -174,6 +176,10 @@ namespace Casino.CardGames.Poker
                 fallbackSwapMessage, ProcessCardSwapStep);
         }
 
+        /// <summary>
+        /// Restarts the game if the player chooses to play again.
+        /// </summary>
+        /// <param name="input">The player's input.</param>
         private void RestartSequence(Input input)
         {
             switch (input)
