8471cd87bc589db75186ac30f02a3877578bfa93
Klipot2 <144228940+Klipot2@users.noreply.github.com>
2024-02-19 12:01:44 +0100

    Update 07_FullHouseInfo.cs
    add summary

--- CardGames/Poker/Combinations/07_FullHouseInfo.cs
+++ CardGames/Poker/Combinations/07_FullHouseInfo.cs
@@ -1,9 +1,20 @@
 namespace Casino.CardGames.Poker.Combinations
 {
+    /// <summary>
+    /// Represents a hand with a full house in a poker hand.
+    /// </summary>
     public class FullHouseInfo : CombinationInfo
     {
-        public FullHouseInfo(List<Card> cards) : base(cards){}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FullHouseInfo"/> class.
+        /// </summary>
+        /// <param name="cards">List of cards in the hand.</param>
+        public FullHouseInfo(List<Card> cards) : base(cards) { }
 
+        /// <summary>
+        /// Checks if the hand contains a full house.
+        /// </summary>
+        /// <returns>True if a full house is present, false otherwise.</returns>
         protected override bool IsCombinationPresent()
         {
             bool hasPair = false;
@@ -17,14 +28,17 @@ namespace Casino.CardGames.Poker.Combinations
             return hasSet && hasPair;
         }
 
+        /// <summary>
+        /// Sorts the cards based on their importance in a full house combination.
+        /// </summary>
         protected override void SortCards()
         {
             base.SortCards();
 
             List<Card> set = PopDuplicates(_cards);
             List<Card> pair = PopDuplicates(_cards);
-            InsertAtFront(pair); 
-            InsertAtFront(set); 
-        }         
+            InsertAtFront(pair);
+            InsertAtFront(set);
+        }
     }
 }
