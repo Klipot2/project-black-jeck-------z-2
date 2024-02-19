2f9fb1faa2770c74e599a8fa5dbe0199f7928d60
Klipot2 <144228940+Klipot2@users.noreply.github.com>
2024-02-19 11:52:38 +0100

    Update 02_PairInfo.cs
    add summary

--- CardGames/Poker/Combinations/02_PairInfo.cs
+++ CardGames/Poker/Combinations/02_PairInfo.cs
@@ -1,17 +1,31 @@
 namespace Casino.CardGames.Poker.Combinations
 {
+    /// <summary>
+    /// Represents a pair in a poker hand.
+    /// </summary>
     public class PairInfo : CombinationInfo
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PairInfo"/> class.
+        /// </summary>
+        /// <param name="cards">List of cards in the hand.</param>
         public PairInfo(List<Card> cards) : base(cards) { }
 
+        /// <summary>
+        /// Checks if the hand contains a pair.
+        /// </summary>
+        /// <returns>True if a pair is present, false otherwise.</returns>
         protected override bool IsCombinationPresent() => CompositionContainsOneOf(new int[] { 2, 3, 4 });
 
+        /// <summary>
+        /// Sorts the cards based on their importance in a pair combination.
+        /// </summary>
         protected override void SortCards()
         {
             base.SortCards();
 
             List<Card> pair = PopDuplicates(_cards);
-            InsertAtFront(pair); 
-        } 
+            InsertAtFront(pair);
+        }
     }
 }
